ee446ad918b5f4d199aaa41634ccda083f1df353
Brian Wangila <bwanjalah@gmail.com>
2022-06-11 18:09:37 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code here!
+// const element = document.createElement("div")
+// document.body.append(element)
+
+// const head = document.createElement("h1")
+// head.textContent = "TEST"
+// element.append(head)
+
+// const textP = document.createElement("p")
+// textP.textContent = "This is a paragraph"
+
+// element.append(textP)
+
+
+// let main = document.getElementById("id")
+// main.innerHTML = "content"
+// main.innerHTML = " <h1>content</h1> "
+
+
+const element = document.getElementById("main")
+element.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");   //OR the below code
+//  newHeader.id = ("victory") //this is similar
+
+newHeader.textContent = "Brian is the champion inside";
+
